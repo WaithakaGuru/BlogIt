e5ed8da325892765666d59a770870f250049ce16
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-09 02:52:49 +0300

    Add user JSON Web Token verifier middleware to authenticate user action

---
+++ backend/src/middlewares/verifyUserJWebToken.middleware.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { configDotenv } from 'dotenv';
+
+configDotenv()
+
+export default async function verifyUserWebToken(req: Request, res: Response, next: NextFunction){
+    const jwtKey = process.env.SECRET_KEY
+    try{
+        const {token} = req.cookies;
+        if (!token || !jwtKey) {
+            res.status(500).json({ message: "No user token found!!" });
+            return
+        }
+        const decodedTokenData = jwt.verify(token,  jwtKey);
+        if(decodedTokenData) next();
+        else return;
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ message: "Something went wrong!!" });
+    }
+}
--- backend/src/routes/Api.router.ts
+++ backend/src/routes/Api.router.ts
@@ -4,13 +4,14 @@ import authenticateUsername from "../middlewares/authenticateUsername.middleware
 import authenticateEmail from "../middlewares/authenticateEmail.middleware.ts";
 import verifyLoginPassword from "../middlewares/verifyLoginPassword.middleware.ts";
 import createUserJWebToken from "../middlewares/createUserWebToken.middleware.ts";
+import verifyUserWebToken from "../middlewares/verifyUserJWebToken.middleware.ts";
 
 const router = Router();
 
 router.post("/auth/register", authenticateUsername, authenticateEmail, createUser);
 router.post("/auth/login", verifyLoginPassword, createUserJWebToken, getAllBlogs)
-router.post("/blogs", createBlog);
+router.post("/blogs",verifyUserWebToken, createBlog);
 router.get("/users", getUsers);
-router.get("/blogs/:id", getSpecificBlog);
-router.get("/blogs", getAllBlogs);
+router.get("/blogs/:id",verifyUserWebToken, getSpecificBlog);
+router.get("/blogs",verifyUserWebToken, getAllBlogs);
 export default router;
