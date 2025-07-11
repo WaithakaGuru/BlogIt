c3bd51888d671ed20691b60a5270d63ec65b8fd6
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-11 10:47:42 +0300

    add the password component with prebuilt text-passwrod type toggle

---
+++ frontend/src/pages/RegisterNewAccountPage.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/material";
+
+function RegisterNewAccountPage () {
+
+    return (
+        <Stack>
+            This is the Signup page
+        </Stack>
+    )
+}
+
+export default RegisterNewAccountPage;
