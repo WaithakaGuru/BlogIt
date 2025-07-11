5ebbddd0c07e0b79e93b5967972223170d14f819
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-11 16:43:42 +0300

    add the delete blog service

---
+++ frontend/src/service/DeleteBlog.ts
@@ -0,0 +1,19 @@
+import useBlog from "../store/Blog.store";
+import { useMutation } from "@tanstack/react-query";
+import axInstance from "../utils/AxInstance";
+
+const deleteBlog = async (blogId: string) => {
+    const {token} = useBlog();
+    const deletedBlog = useMutation({
+        mutationKey: ["DELETE_BLOG", blogId],
+        mutationFn: async () => {
+            await axInstance.patch(`/blogs/${blogId}`, {
+                headers:{
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+        }
+    })
+    return deletedBlog;
+}
+export default deleteBlog
