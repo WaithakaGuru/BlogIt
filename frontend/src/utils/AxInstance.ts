d516d9a3fd43e1abd963019b24ed3aa5012bea26
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-14 16:59:08 +0300

    add external backend URL

--- frontend/src/utils/AxInstance.ts
+++ frontend/src/utils/AxInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axInstance = axios.create({
-  baseURL: "http://localhost:4556/api",
+  // baseURL: "http://localhost:4556/api",
+  baseURL: "https://blogit-backend-c286.onrender.com/",
   withCredentials: true,
 });
 export default axInstance;
