42dfd3e5479403f38f55436034e1ea3ca017c45d
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-16 02:08:45 +0300

    Fix backend URL

--- frontend/src/utils/AxInstance.ts
+++ frontend/src/utils/AxInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axInstance = axios.create({
-  baseURL: "http://localhost:4556/api",
-  // baseURL: "https://blogit-backend-c286.onrender.com/api/",
+  // baseURL: "http://localhost:4556/api",
+  baseURL: "https://blogit-backend-c286.onrender.com/api/",
   withCredentials: true,
 });
 export default axInstance;
